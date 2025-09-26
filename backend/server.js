428223f2dc9dae9a72a47bcc52e17cda6de83906
Paul Benedict Medina <paulmedina645@gmail.com>
2025-09-27 01:43:31 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://rabbit-2-ecommerce.vercel.app", // frontend url
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
